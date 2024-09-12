b85227f67a086458685b69e627b7810ce0a743aa
prabhat <prabhatshukla0849@gmail.com>
2024-09-12 08:13:55 +0530

    updated schema

--- app/createStream/page.tsx
+++ app/createStream/page.tsx
@@ -1,27 +1,35 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import {SubmitHandler, useForm} from "react-hook-form"
+import {set, SubmitHandler, useForm} from "react-hook-form"
+import { z } from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
 
-type FormFields = {
-    email: string,
-    password: string
-}
+const FormObject = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+})
+
+type FormFields = z.infer<typeof FormObject>
 
 export default function Create(){
     
-   const { register , handleSubmit , formState: {errors,isSubmitting}} = useForm <FormFields>()
+   const { register , handleSubmit , setError, formState: {errors,isSubmitting}} = useForm <FormFields>({
+    defaultValues:{
+        email:"test@gmail.com"
+    },
+    resolver: zodResolver(FormObject)
+   })
 
    const onSubmit: SubmitHandler<FormFields> = async(data) =>{
-
-    await new Promise((resolve) => setTimeout( resolve , 2000));
-    console.log(data);
-    
-    
-   }
-
-     
-    
-
+    try{
+        await new Promise((resolve) => setTimeout( resolve , 2000));
+        throw new Error()
+        console.log(data);
+    }catch(error){
+        setError("root",{
+            message:"email is already taken"
+    })
+    }}
 
     return (
         <div className="flex flex-col justify-between items-center divide-y divide-red-400">
@@ -29,26 +37,11 @@ export default function Create(){
             <div className="text-3xl font-bold text-cyan-500"> Create a Stream</div>
             
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-6 border-s-violet-600">
-                <input {...register("email" ,{
-                    required: "Email is required",
-                    validate:(value) => {
-                        if(!value.includes("@")){
-                            return "Invalid email ,it must contain @"
-                        }
-                        return true;
-                    },
-                })} type="text" placeholder="email"/>
+                <input {...register("email")} type="text" placeholder="email"/>
 
                 {errors.email && <div className="text-red-600">{errors.email.message}</div>} 
 
-                <input {...register("password",{
-                    required:"Password is required",
-                    minLength:{
-                        value:8,
-                        message:"Password must be at least 8 characters long"
-                        
-                    }
-                })} type='text' placeholder="password"/>
+                <input {...register("password")} type='text' placeholder="password"/>
 
                 {errors.password && <div className="text-red-600">{errors.password.message}</div>}
 
@@ -60,7 +53,10 @@ export default function Create(){
                     </Button>
                 <Button variant={"outline"} type="reset"> Reset</Button>
 
+
+
                 </div>
+                {errors.root && <div className="text-red-600">{errors.root.message}</div>}
                
             </form>
           
