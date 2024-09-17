78397c54f5d9bc8e8b8ed7fe10cccbed52af6b85
prabhat <prabhatshukla0849@gmail.com>
2024-09-17 20:50:06 +0530

    error handling

--- app/api/override/route.ts
+++ app/api/override/route.ts
@@ -3,7 +3,7 @@ import { db } from "@/lib/db";
 
 export async function POST(req: Request){
 
-
+ try{
     const body = await req.json()
 
     const {date} = body
@@ -27,4 +27,10 @@ export async function POST(req: Request){
     })
     
 
+
+ }catch(error){
+    return new Response("fuck")
+
+ }
+    
 }
