bfb70840e0cff10e83173d382e228cbbeb9f1d9f
prabhat <prabhatshukla0849@gmail.com>
2024-09-17 18:31:07 +0530

    override model added

--- app/api/override/route.ts
+++ app/api/override/route.ts
@@ -1,3 +1,38 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+
 export async function POST(req: Request){
+
+
+    const body = await req.json()
+
+    const {date} = body
+
+    const session = await getAuthSession()
+
+    if(!session?.user){
+
+    return new Response("unauthorized access",
+        {
+            status:401,
+        }
+    ) 
+
+    await db.override.create({
+        data:{
+            date:date
+        }
+        where:{
+            user.id: session.userId
+        }
+    })
     
+    
+    
+    
+
+
+
+   }
+
 }
