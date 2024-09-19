ca7d85505ed92baca7fccc11f737255ce184d0f3
prabhat <prabhatshukla0849@gmail.com>
2024-09-19 06:20:16 +0530

    some schema changes

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,4 +1,5 @@
 import { getAuthSession } from "@/lib/auth"
+import { db } from "@/lib/db"
 
 export async function POST(req: Request){
     const body = await req.json()
@@ -11,6 +12,13 @@ export async function POST(req: Request){
         return new Response("User not Authorized", { status: 401 })
     }
 
-    
+    await db.event.create({
+        data:{
+            duration: duration,
+            userId: session.user.id
+        }
+    })
+
+
 
 }
