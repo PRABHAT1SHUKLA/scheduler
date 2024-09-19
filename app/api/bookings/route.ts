1cb48693ef76b227dd1fba589fbd227cae568c80
prabhat <prabhatshukla0849@gmail.com>
2024-09-19 07:13:56 +0530

    booking route implementation

--- app/api/bookings/route.ts
+++ app/api/bookings/route.ts
@@ -1,3 +1,13 @@
+import { getAuthSession } from "@/lib/auth"
+
 export async function POST(req:Request){
-    const {name , email , note , status}
+    const body = await req.json()
+
+    const {name , email , note , status ,date , start , end } = body
+
+    const session = await getAuthSession()
+
+    if(!session?.user){
+        return {status: 401, message: 'Unauthorized'}
+    }
 }
