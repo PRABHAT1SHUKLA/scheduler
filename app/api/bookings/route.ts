77f895b322aa18ec9c47a8905fef478f4e35e4b1
prabhat <prabhatshukla0849@gmail.com>
2024-09-20 09:38:08 +0530

    logic of bookings route

--- app/api/bookings/route.ts
+++ app/api/bookings/route.ts
@@ -1,13 +1,28 @@
 import { getAuthSession } from "@/lib/auth"
+import { db } from "@/lib/db"
 
 export async function POST(req:Request){
     const body = await req.json()
 
-    const {name , email , note , status ,date , start , end } = body
+    const {name , email , note , status ,date , start , end, userId} = body
 
     const session = await getAuthSession()
 
     if(!session?.user){
         return {status: 401, message: 'Unauthorized'}
     }
+
+    await db.bookings.create({
+        data:{
+            userId: userId,
+            clientName: name,
+            client
+        }
+    })
+
+
+    
+    
+
+    
 }
