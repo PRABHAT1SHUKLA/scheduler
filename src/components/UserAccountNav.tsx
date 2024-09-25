60e4df42334128a7b45328b053a701f303215060
prabhat <prabhatshukla0849@gmail.com>
2024-09-25 15:20:40 +0530

    ui changes

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,26 +1,34 @@
 
 import React from "react"
-import { DropdownMenu,DropdownMenuItem,DropdownMenuSeparator,DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuSeparator,DropdownMenuTrigger } from "./ui/dropdown-menu"
 import { LogOut } from "lucide-react"
 import Logout from "./LogOut"
+import { User } from "next-auth"
 
 interface userAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
 
   user: Pick<User,"image"|"email"|"name">
 }
 
-export function  UserAccontNav ({user}: userAccountNavProps){
-  return(<>
+export function  UserAccontNav({user}: userAccountNavProps){
+  return(
+  <>
     <DropdownMenu>
-     <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
      <DropdownMenuItem>helllo</DropdownMenuItem>
      <DropdownMenuSeparator/>
      <DropdownMenuItem>whtsup</DropdownMenuItem>
      <DropdownMenuSeparator/>
      <DropdownMenuItem><LogOut/><Logout/></DropdownMenuItem>
-    </DropdownMenu>
+  
+      </DropdownMenuContent>
+      </DropdownMenu>
+     
 
   
-  </>)
+  </>
+  )
 }
 
