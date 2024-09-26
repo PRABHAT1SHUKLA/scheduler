647868229637fd152c5d682bfdf52789edb630c2
prabhat <prabhatshukla0849@gmail.com>
2024-09-26 09:21:30 +0530

    some changes in folder structure

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+   
+    route: "/",
+    label: "Home",
+  },
+  {
+    
+    route: "/Tametime/availability",
+    label: "admin availability",
+  },
+  {
+   
+    route: "/Tametime/event-type",
+    label: "event type",
+  },
+  {
+   
+    route: "/Tametime/bookings",
+    label: "admin bookings",
+  },
+];
