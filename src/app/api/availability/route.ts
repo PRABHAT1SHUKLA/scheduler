8651e859659f83c4d59305f6b530a9ba37bc0f11
prabhat <prabhatshukla0849@gmail.com>
2024-09-28 19:42:46 +0530

    availability creation route created

--- src/app/api/availability/route.ts
+++ src/app/api/availability/route.ts
@@ -5,7 +5,7 @@ import { availabilitySchema } from "@/lib/validators/availability"
 import { NextApiRequest, NextApiResponse } from "next"
 
 //route handles admin request for availability creation
-export async function POST(req: NextApiRequest,res:NextApiResponse) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
     const session = await getAuthSession()
 
@@ -16,74 +16,78 @@ export async function POST(req: NextApiRequest,res:NextApiResponse) {
     }
 
     const parsedData = availabilitySchema.parse(req.body);
- 
+
     const user = await db.user.findUnique({
-      where:{
-        id: session.user.id 
+      where: {
+        id: session.user.id
       },
-      include:{
+      include: {
         availability: true
       }
     })
-     
 
-    if(!user){
+
+    if (!user) {
       return res.status(404).json({
         error: "User not found"
       })
     }
 
-   const availabilityData = Object.entries(parsedData).flatMap(
-    ([day,{isAvailable,startTime,endTime}]) =>{
-        if(isAvailable){
+    const availabilityData  = Object.entries(parsedData).flatMap(
+      ([day, { isAvailable, startTime, endTime }]) => {
+        if (isAvailable) {
           const baseDate = new Date().toISOString().split('T')[0];
 
-          return[
+          return [
             {
               day: day.toUpperCase(),
-              startTime: new Date(`${baseDate}T${startTime}`).toISOString(),
-              endTime:new Date(`${baseDate}T${endTime}:00Z`),
+              startTime: new Date(`${baseDate}T${startTime}:00Z`),
+              endTime: new Date(`${baseDate}T${endTime}:00Z`),
             },
           ];
         }
-        return[];
+        return [];
+      }
+    );
+
+    //To update availability data by doing a db call  here
+
+    if (user.availability) {
+
+      await db.weeklyavailability.update({
+        where: { id: user.availability.id },
+        data: {
+          timeGap: parsedData.timeGap,
+          days: {
+            deleteMany: {},//deletes all existing data
+            create: availabilityData,
+          }
+        }
+      })
+    } else {
+      await db.weeklyavailability.create({
+        data: {
+          userId: user.id,
+          timeGap: parsedData.timeGap,
+          days: {
+            create: availabilityData,
+          }
+
+        }
+      })
     }
-   );
-
-   //To update availability data by doing a db call  here
-   
-   if(user.availability){
-
-    await db.weeklyavailability.update({
-      where:{ id: user.availability.id},
-      data:{
-        timeGap: parsedData.timeGap,
-        days:{
-          deleteMany:{},//deletes all existing data
-          create: availabilityData,
-        }
-      }
-    })
-   } else{
-    await db.weeklyavailability.create({
-      data:{
-        userId: user.id,
-        timeGap: parsedData.timeGap,
-        days:{
-          create: availabilityData,
-        }
-
-      }
-    })
-   }
 
 
-
+  return res.status(200).json({success:true});
 
 
   }
-  catch (error: any) {
-    return new Response(error)
+  catch (error) {
+    console.error('Error updating availability:', error);
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ error: 'Invalid request data', details: error.errors });
+    }
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
 
@@ -91,7 +95,7 @@ export async function POST(req: NextApiRequest,res:NextApiResponse) {
 
 export async function GET(req: Request) {
   try {
-   
+
     const session = await getAuthSession()
 
 
@@ -108,17 +112,17 @@ export async function GET(req: Request) {
       },
       include: {
         availability: {
-          include:{ days:true}
+          include: { days: true }
         }
       },
     })
 
-    if(!user || !user?.availability) {
+    if (!user || !user?.availability) {
       return null
     }
-  
+
     const data = {
-      timeGap : user.availability.timeGap
+      timeGap: user.availability.timeGap
     };
 
     [
@@ -129,21 +133,21 @@ export async function GET(req: Request) {
       "friday",
       "saturday",
       "sunday",
-  ].forEach((day) => {
-    const dayAvailability = user.availability?.days.find((d) => d.day === day.toUpperCase());
+    ].forEach((day) => {
+      const dayAvailability = user.availability?.days.find((d) => d.day === day.toUpperCase());
 
-     data[day] ={
-      isAvailable : !!dayAvailability,
-      startTime: dayAvailability? dayAvailability.startTime.toISOString().slice(11,16)
-      : "09:00",
-      endTime: dayAvailability? dayAvailability.endTime.toISOString().slice(11,16): "17.00"
-     }
+      data[day] = {
+        isAvailable: !!dayAvailability,
+        startTime: dayAvailability ? dayAvailability.startTime.toISOString().slice(11, 16)
+          : "09:00",
+        endTime: dayAvailability ? dayAvailability.endTime.toISOString().slice(11, 16) : "17.00"
+      }
 
-     
 
-  })
 
-  return data;
+    })
+
+    return data;
 
 
   }
