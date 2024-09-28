5c8226a6583535903f1e064ce36a8c6d2c251563
prabhat <prabhatshukla0849@gmail.com>
2024-09-28 15:38:41 +0530

    created route for getuseravailability

--- src/app/api/availability/route.ts
+++ src/app/api/availability/route.ts
@@ -46,23 +46,53 @@ export async function GET(req: Request) {
       })
     }
 
-    const user = await db.user.findFirst({
+    const user = await db.user.findUnique({
       where: {
         id: session.user.id,
       },
       include: {
-        availability: true
+        availability: {
+          include:{ days:true}
+        }
       },
     })
 
-    if(!user) {
-      return new Response ("user not found ", {
-        status: 404
-      })
+    if(!user || !user?.availability) {
+      return null
     }
+  
+    const data = {
+      timeGap : user.availability.timeGap
+    };
 
-    return NextResponse.json(user.availability)
+    [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday",
+  ].forEach((day) => {
+    const dayAvailability = user.availability?.days.find((d) => d.day === day.toUpperCase());
+
+     data[day] ={
+      isAvailable : !!dayAvailability,
+      startTime: dayAvailability? dayAvailability.startTime.toISOString().slice(11,16)
+      : "09:00",
+      endTime: dayAvailability? dayAvailability.endTime.toISOString().slice(11,16): "17.00"
+     }
+
+     
+
+  })
+
+  return data;
+
+
   }
+
+
 
   catch (error: any) {
     return new Response(error)
