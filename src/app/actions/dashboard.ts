6f9d783330072e52c8bc458d0a25de2736d32787
prabhat <prabhatshukla0849@gmail.com>
2024-09-30 17:55:59 +0530

    created route for updating dashboard based on the latest booking data

--- src/app/actions/dashboard.ts
+++ src/app/actions/dashboard.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { getAuthSession } from "@/lib/auth"
+import { db } from "@/lib/db";
 
 export async function getLatestUpdates(){
   const session  = await getAuthSession()
@@ -10,6 +11,34 @@ export async function getLatestUpdates(){
   }
 
   const user = await db.user.findUnique({
-    
+    where:{
+         id: session.user.id
+    }
   })
+
+  if(!user){
+    throw new Error("user not found")
+  }
+
+  const current = new Date();
+
+  const UpcomingMeetings = await db.booking.findMany({
+    where:{
+      userId: user.id,
+      startTime:{gte:current},
+    },
+    include:{
+      event:{
+        select:{
+          title:true,
+        },
+      },
+    },
+    orderBy:{
+      startTime:"asc",
+    },
+    take:3,
+  });
+
+  return UpcomingMeetings;
 }
