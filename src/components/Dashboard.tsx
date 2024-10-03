cde843609eb665cb3978728c5a8a0fd4e4348e6a
prabhat <prabhatshukla0849@gmail.com>
2024-10-03 08:16:10 +0530

    user dashboard component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,14 @@
 "use client"
 
+import { UsernameValidator } from "@/lib/validators/username";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
+
+
+type FormData = z.infer<typeof UsernameValidator>
+
 
 export default function Dashboardpage(){
 
@@ -23,7 +30,39 @@ export default function Dashboardpage(){
     }
   }, [session]);
 
-  
+  const { mutate: updateUsername , isLoading} =
+  useMutation({
+    mutationFn :async ({ name} : FormData) =>{
+      const payload: FormData = {name}
+
+      const {data } = await axios.post("/api/username", payload)
+      return data
+
+    },
+    onError:(err) =>{
+      if (err instanceof AxiosError){
+         if(err.response?.status === 409){
+          return toast({
+            title: 'Username already taken.',
+            description: 'Please choose another username.',
+            variant: 'destructive',
+          })
+         }
+      }
+       
+      return toast({
+        title: 'Something went wrong.',
+        description: 'Your username was not updated. Please try again.',
+        variant: 'destructive',
+      })
+   
+
+
+    }
+
+
+
+  })
   
    
 
